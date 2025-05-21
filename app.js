1412fdecbe178c4ae2775536c9da432d895f6f11
Spcoderr <sajithpriyashan105@gmail.com>
2025-05-21 13:03:26 +0530

    database configure

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+connectDB();
+
 
 app.use(cors());
 app.use(express.urlencoded({
@@ -9,4 +15,6 @@ app.use(express.urlencoded({
  }));
 app.use(express.json());
 
+
 module.exports = app;
+
